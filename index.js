ed76835f90190eb068841bea837d499c479ae62c
Narak <nrraiyugin@gmail.co>
2026-04-27 11:25:08 +0700

    change in main

---
+++ index.js
@@ -0,0 +1,14 @@
+// IMPORT Express Server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1>It is working now.</h1>")
+})
+
+app.post('/', (req, res)=> {
+    res.send("Received!")
+})
+
+app.listen(5000, () =>
+console.log('EXPRESS Server Started at Port No: 5000'));
